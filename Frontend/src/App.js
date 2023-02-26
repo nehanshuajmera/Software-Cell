064f2268263b94c7df6585d4172a4b6cf9ee7ffa
arnav bahadur <bahadurarnav2203@gmail.com>
2023-02-26 23:29:03 +0530

    about

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,15 +6,6 @@ import Preloader from './Components/Preloader';
 import Software from './Components/Software';
 import Layout from './Components/Layout';
 
-<<<<<<< HEAD
-import Navbar from './Components/Navbar';
-=======
->>>>>>> e9e1ea8e3ff0e97218013f96061d2619af9d16d5
-// import Aboutus from './Pages/Aboutus';
-// import Contactus from './Pages/Contactus';
-// import Home from './Pages/Home';
-// import Ourteam from './Pages/Ourteam';
-// import Footer from './Components/Footer';
 
 function App() {
   const [loading, setloading] = useState(false)
@@ -25,35 +16,16 @@ function App() {
   return (
     
    <div>
- <Preloader/>
- <Software/>
-<<<<<<< HEAD
- <Navbar/>
-{/* <Home/> */}
-=======
+    {!loading ?
+    <> 
+    <Preloader />
+ <Software />
  </>
- :
-  <>
-  <Layout/>  
-  </>
-  }
-
-  
-
-
-
- 
->>>>>>> e9e1ea8e3ff0e97218013f96061d2619af9d16d5
-
-    {/* <Navbar/>
-    <Home/>  */}
-  {/* <Contactus/>  */}
-  {/* <Aboutus/> */}
-
-      {/* <Footer/>
-    <Aboutus/>  */}
-   {/* <Ourteam/>  */}
-    
+:
+<>
+<Layout />
+</>
+}
    </div>
   );
 }
--- Frontend/src/Pages/About.jsx
+++ Frontend/src/Pages/About.jsx
@@ -3,9 +3,274 @@ import './About.css'
 
 const About = () => {
   return (
-    <div>
-      About
+    <>
+    <title>Animate On Scroll | AOS</title>
+      <div className="aboutusmainbody">
+        
+  <div className="all-padding">
+  <div className="heading"><h2>-Who Are We-</h2></div>
+  <div className="content">
+  Medi-Caps University has established a Software Cell with the objective of enhancing the skills and knowledge of its students in the field of software development.
+The Software Cell at Medi-Caps University is committed to equipping students with the latest tools and techniques in software development. Its primary aim is to bridge the gap between industry requirements and academic curriculums by organising various training sessions and providing internships. Additionally, the Software Cell offers students opportunities to work on real-world projects, thereby enabling them to gain practical experience in the software development field.
+
+The Software Cell at Medi-Caps University comprises a team of experienced and dedicated members who are committed to nurturing the talent of students and guiding them in their journey towards becoming successful software professionals.
+In conclusion, the Software Cell serves as an exceptional platform that empowers students to explore their potential and excel in this field. It is a hub of innovation and excellence that prepares students to meet the demands of ever-evolving technology.
+  </div>
+  <div className="container">
+    {/*-----------------------------------------------------------*/}
+    <div
+      className="div1 persontalk"
+      data-aos="fade-left"
+      data-aos-duration={1000}
+      data-aos-once="true"
+    >
+      <img
+        src="https://www.medicaps.ac.in/upload/medicapsuniv/page/1621580768.jpeg"
+        alt="person1"
+        className="personImg-talked"
+      />
+      <div className="content">
+        <p>
+          I am enormously delighted to welcome you all to being a part of the
+          Medi-Caps University family. I plead to the Almighty to bless you with
+          an excellent future ahead.
+        </p>
+      </div>
     </div>
+    {/*----------------------------------------------------------*/}
+    <div className="div2 persontalk" data-aos="fade-right">
+      <img
+        src="https://www.medicaps.ac.in/upload/medicapsuniv/page/1625643116.jpg"
+        alt="person2"
+        className="personImg-talked"
+      />
+      <div className="content">
+        <p>
+          Our mission is to make our students well-equipped at various levels of
+          Graduate and Post Graduate degrees with an outstanding CBCS curriculum
+          and an unprecedented teaching-mentoring pedagogy.
+        </p>
+      </div>
+    </div>
+    {/* -----------------------------------------------------------*/}
+    <div className="div3 persontalk" data-aos="fade-up">
+      <img
+        src="https://static01.nyt.com/images/2021/10/13/science/13shatner-launch-oldest-person-sub/13shatner-launch-oldest-person-sub-superJumbo.jpg"
+        alt="person3"
+        className="personImg-talked"
+      />
+      <div className="content">
+        <p>
+          Lorem 3 ipsum dolor sit amet consectetur adipisicing elit. Ad, veniam?
+        </p>
+      </div>
+    </div>
+  </div>
+  <div className="content">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores aut
+    adipisci, fuga, quia animi vitae saepe ullam autem, atque voluptas fugit ut
+    nam minus obcaecati reprehenderit impedit cum rem laborum similique.
+    Officiis nihil assumenda mollitia itaque voluptate soluta debitis optio, vel
+    accusantium! Provident totam quae autem consequatur consectetur. Iure,
+    cumque.
+  </div>
+
+</div>
+      <div>
+        
+          <div className='aboutus-pic'>
+          
+        <div className="wrapper">
+          
+          <img className="diamond" src="https://source.unsplash.com/random/600x600?water" alt="" />
+          <img className="diamond" src="https://source.unsplash.com/random/600x600?summer" alt="" />
+          <img className="diamond" src="https://source.unsplash.com/random/600x600?plants" alt="" />
+          <img className="diamond" src="https://source.unsplash.com/random/600x600?snow" alt="" />
+          <img className="diamond" src="https://source.unsplash.com/random/600x600?roses" alt="" />
+          <img className="diamond" src="https://source.unsplash.com/random/600x600?sky" alt="" />
+          <img className="diamond" src="https://source.unsplash.com/random/600x600?nature" alt="" />
+          <img className="diamond" src="https://source.unsplash.com/random/600x600?blossom" alt="" />
+          <img className="diamond" src="https://source.unsplash.com/random/600x600?ice" alt="" />
+          <img className="diamond" src="https://source.unsplash.com/random/600x600?spring" alt="" />
+        </div>
+      </div>
+      </div>
+      <div className="wrapper-2">
+  <h1 className="heading">Our Team</h1>
+  <div className="cols">
+    <div className="col" ontouchstart="this.classList.toggle('hover');">
+      <div className="container-hover">
+        <div
+          className="front"
+          style={{ backgroundImage: "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_Bvr7b5333hkIr_2VwrdlHsYlKh_Mb0ZRHBBAlHhLRMVCkgSaxadUHrFQkSoupUyj-BY&usqp=CAU)" }}
+        >
+          <div className="inner">
+            <p>Diligord</p>
+            <span>Lorem ipsum</span>
+          </div>
+        </div>
+        <div className="back">
+          <div className="inner">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias
+              cum repellat velit quae suscipit c.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="col" ontouchstart="this.classList.toggle('hover');">
+      <div className="container-hover">
+        <div
+          className="front"
+          style={{ backgroundImage: "url(https://unsplash.it/511/511/)" }}
+        >
+          <div className="inner">
+            <p>Rocogged</p>
+            <span>Lorem ipsum</span>
+          </div>
+        </div>
+        <div className="back">
+          <div className="inner">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias
+              cum repellat velit quae suscipit c.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="col" ontouchstart="this.classList.toggle('hover');">
+      <div className="container-hover">
+        <div
+          className="front"
+          style={{ backgroundImage: "url(https://unsplash.it/502/502/)" }}
+        >
+          <div className="inner">
+            <p>Strizzes</p>
+            <span>Lorem ipsum</span>
+          </div>
+        </div>
+        <div className="back">
+          <div className="inner">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias
+              cum repellat velit quae suscipit c.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="col" ontouchstart="this.classList.toggle('hover');">
+      <div className="container-hover">
+        <div
+          className="front"
+          style={{ backgroundImage: "url(https://unsplash.it/503/503/)" }}
+        >
+          <div className="inner">
+            <p>Clossyo</p>
+            <span>Lorem ipsum</span>
+          </div>
+        </div>
+        <div className="back">
+          <div className="inner">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias
+              cum repellat velit quae suscipit c.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="col" ontouchstart="this.classList.toggle('hover');">
+      <div className="container-hover">
+        <div
+          className="front"
+          style={{ backgroundImage: "url(https://unsplash.it/504/504/)" }}
+        >
+          <div className="inner">
+            <p>Rendann</p>
+            <span>Lorem ipsum</span>
+          </div>
+        </div>
+        <div className="back">
+          <div className="inner">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias
+              cum repellat velit quae suscipit c.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="col" ontouchstart="this.classList.toggle('hover');">
+      <div className="container-hover">
+        <div
+          className="front"
+          style={{ backgroundImage: "url(https://unsplash.it/505/505/)" }}
+        >
+          <div className="inner">
+            <p>Reflupper</p>
+            <span>Lorem ipsum</span>
+          </div>
+        </div>
+        <div className="back">
+          <div className="inner">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias
+              cum repellat velit quae suscipit c.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="col" ontouchstart="this.classList.toggle('hover');">
+      <div className="container-hover">
+        <div
+          className="front"
+          style={{ backgroundImage: "url(https://unsplash.it/506/506/)" }}
+        >
+          <div className="inner">
+            <p>Acirassi</p>
+            <span>Lorem ipsum</span>
+          </div>
+        </div>
+        <div className="back">
+          <div className="inner">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias
+              cum repellat velit quae suscipit c.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="col" ontouchstart="this.classList.toggle('hover');">
+      <div className="container-hover">
+        <div
+          className="front"
+          style={{ backgroundImage: "url(https://unsplash.it/508/508/)" }}
+        >
+          <div className="inner">
+            <p>Sohanidd</p>
+            <span>Lorem ipsum</span>
+          </div>
+        </div>
+        <div className="back">
+          <div className="inner">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias
+              cum repellat velit quae suscipit c.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+      </div>
+   </>
   )
 }
 
